a5b45e8c562ff674db5b9188037f1d32f838108b
Fer Torres <fertorresmx@gmail.com>
2021-02-21 17:46:44 -0600

    Get the body from first four transcriptions (one page)

--- wordcloud_conferences/wordcloud_conferences/spiders/transcriptions.py
+++ wordcloud_conferences/wordcloud_conferences/spiders/transcriptions.py
@@ -25,28 +25,38 @@ class GetTranscriptionsSpider(scrapy.Spider):
         'USER_AGENT': 'FerTorres'
     }
 
-    # Título = //div[contains(@class, "category-version-estenografica")]//h3[@class="entry-title"]/a/text()
-    # link = //div[contains(@class, "category-version-estenografica")]//h3[@class="entry-title"]/a/@href
-
-    # next page link = //div[@class="pagenavbar"][2]/div/span[@class="page-numbers current"]/following-sibling::*[1]/@href
 
     def parse(self, response):
+        # Short title = //div[contains(@class, "category-version-estenografica")]//h3[@class="entry-title"]/a/text()
+        # url = //div[contains(@class, "category-version-estenografica")]//h3[@class="entry-title"]/a/@href
+        # Next page link = //div[@class="pagenavbar"][2]/div/span[@class="page-numbers current"]/following-sibling::*[1]/@href
 
         posts = response.xpath('//div[contains(@class, "category-version-estenografica")]//h3[@class="entry-title"]')
 
         for post in posts:
-
             short_title = post.xpath('a/text()').get()
-            link = post.xpath('a/@href').get()
+            url = post.xpath('a/@href').get()
 
-            yield {
-                'short_title': short_title,
-                'link': link
-            }
+            yield response.follow( url, callback=self.parse_url, cb_kwargs={'short_title': short_title, 'url': url} )
 
-            next_page_button_link = response.xpath('//div[@class="pagenavbar"][last()]/div/span[@class="page-numbers current"]/following-sibling::*[1]/@href').get()
-
+            # next_page_button_link = response.xpath('//div[@class="pagenavbar"][last()]/div/span[@class="page-numbers current"]/following-sibling::*[1]/@href').get()
+            next_page_button_link = False
             if next_page_button_link:
                 yield response.follow(next_page_button_link, callback=self.parse)
 
 
+    def parse_url(self, response, **kwargs):
+        # Title: //header[@class="entry-header"]/h2[@class="entry-title"]/text()
+        #Body: //div[@class="entry-content"]/p #With tags p and strong for the speakers
+
+        short_title = kwargs['short_title']
+        title = response.xpath('//header[@class="entry-header"]/h2[@class="entry-title"]/text()').get()
+        url = kwargs['url']
+        body = response.xpath('//div[@class="entry-content"]/p').getall()
+
+        yield {
+            'short_title': short_title,
+            'title': title,
+            'url': url,
+            'body': body
+        }
